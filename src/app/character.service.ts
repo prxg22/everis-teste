fe9557a9845ab1d88b95efc0d085f55ac28b6699
Paulo Ricardo Xavier Giusti <paulo.giusti@meiuca.design>
2018-07-30 18:18:44 -0300

    gh pages

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -8,7 +8,7 @@ import { Char } from './interfaces'
   providedIn: 'root'
 })
 export class CharacterService {
-  url: string = 'http://www.mocky.io/v2/5b2971722f00006300f561a9'
+  url: string = 'https://www.mocky.io/v2/5b2971722f00006300f561a9'
   constructor(
     private http: HttpClient
   ) { }
